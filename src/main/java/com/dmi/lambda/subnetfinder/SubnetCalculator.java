6d31fa533e3b404105dae205115cb2d2ddb93b46
Musema.Hassen <Musema.Hassen@CORP-L-74601JM.adesa.com>
2018-03-10 21:46:42 -0500

    Implement database services

--- src/main/java/com/dmi/lambda/subnetfinder/SubnetCalculator.java
+++ src/main/java/com/dmi/lambda/subnetfinder/SubnetCalculator.java
@@ -1,44 +1,71 @@
 package com.dmi.lambda.subnetfinder;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class SubnetCalculator implements RequestHandler<LinkedHashMap<String,Object>, LambdaCFNResponse> {
 	private LambdaLogger logger;
+	private DatabaseServices dbService;
+	private LambdaCFNRequest request=new LambdaCFNRequest();
+	private LambdaCFNResponse response=new LambdaCFNResponse();
     @Override
     public LambdaCFNResponse handleRequest(LinkedHashMap<String,Object> input, Context context) {
-    	logger=context.getLogger();
-        logger.log("Input: " + input.toString());
-        
-        logger.log("\n");
-       for(Map.Entry<String, Object> en:input.entrySet()){
-    	   if(en.getKey().equalsIgnoreCase("ResourceProperties"))
-    		   continue;
-    	   logger.log(en.getKey()+","+en.getValue()+"\n");
-    	   logger.log("Entry class:"+en.getClass()+"\n");
-       }
+       logger=context.getLogger();
        
-       LambdaCFNResponse response=new LambdaCFNResponse();
-       response.setStatus("Success");
-       response.setLogicalResourceId("id-1");
-       response.setData(new HashMap<String,String>());
-       response.getData().put("key1", "val1");
-       response.getData().put("key2", "val2");
-       return response;
-        //return transformInputToCFNRequest(input);
+       this.request=transformInputToCFNRequest(input);
+       String networkAdress=request.getResourceProperties().get("vpc");
+       if(networkAdress==null){
+    	   this.response.setStatus("Failure");
+    	   this.response.getData().put("Reason", "Invalid or no network address is provided");
+    	   
+    	   logger.log("\nUnable to calculate the subnet without knowing the network address\n");
+    	   return null;
+       }
+       AmazonDynamoDB client=AmazonDynamoDBClientBuilder.standard().withRegion(Regions.fromName("us-east-1")).build();
+       this.dbService=new DatabaseServices(client);
+       
+       if(request.getRequestType().equalsIgnoreCase("Create")){
+    	   this.response.setData(IpUtils.allocateSubnets(request,networkAdress, dbService, 4));
+    	   populateCommonCFNResponse();
+    	   this.response.setStatus("Success");
+    	   return this.response;
+       }
+       if(request.getRequestType().equalsIgnoreCase("Delete")){
+    	   this.dbService.deAllocateSubnets(this.request.getStackId());
+    	   populateCommonCFNResponse();
+    	   this.response.setStatus("Success");
+    	   return this.response;
+       }
+       if(request.getRequestType().equalsIgnoreCase("Update")){
+    	   //update operation
+    	   populateCommonCFNResponse();
+    	   this.response.setStatus("Success");
+    	   return this.response;
+       }
+       this.response.setStatus("Failure");
+	   this.response.getData().put("Reason", "Unknown");
+       return this.response;
     }
-    public LambdaCFNRequest transformInputToCFNRequest(LinkedHashMap<String,Object> input){
-    	LambdaCFNRequest request=new LambdaCFNRequest();
-    	 for(Map.Entry<String, Object> en:input.entrySet()){
+    @SuppressWarnings("unchecked")
+	public LambdaCFNRequest transformInputToCFNRequest(LinkedHashMap<String,Object> input){
+    	for(Map.Entry<String, Object> en:input.entrySet()){
     		 if(en.getKey().equalsIgnoreCase("ResourceProperties"))
-      		   continue;
-    		 request.setValue(en.getKey(), en.getValue());
+      		   {
+    			 this.request.setResourceProperties((Map<String, String>)en.getValue());
+    			 continue;
+      		   }
+    		 this.request.setValue(en.getKey(), en.getValue());
          }
-    	return request;
+    	return this.request;
+    }
+    public void populateCommonCFNResponse(){
+    	
     }
 }
